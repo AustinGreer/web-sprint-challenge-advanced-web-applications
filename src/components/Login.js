31b9ba580d5a53f93cbe3c5970ee01e78f8eb357
Austin Greer <agreer.ag12@gmail.com>
2021-04-23 07:17:31 -0500

    Login form complete. On successful login, token is saved to local storage and user is redirected to BubblePae. Form error is also handled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios'
 
 const initialFormValues = {
   username: '',
   password: ''
 }
 
-const initialErrorValues = {
-  username: '',
-  password: ''
-}
-
 const Login = () => {
   const [formValues, setFormValues] = useState(initialFormValues)
-  const [formErrors, setformErrors] = useState('')
+  const [formError, setFormError] = useState('')
+  const { push } = useHistory()
 
   const inputChangeHandler = e => {
     setFormValues({
@@ -21,18 +19,31 @@ const Login = () => {
     })
   }
 
-  useEffect(()=>{
-    // make a post request to retrieve a token from the api
-    // when you have handled the token, navigate to the BubblePage route
-  });
-  
-  
+  // project was initially using the useEffect hook for post request
+  // I changed it to a submit handler because we want post request to fire when form is submitted
+  const loginHandler = e => {
+    e.preventDefault()
+
+    axios
+      .post('http://localhost:5000/api/login', formValues)
+      .then(res => {
+        window.localStorage.setItem('token', res.data.payload) // note - I am not going to stringify since payload comes back as string
+        push('/bubblepage')
+      })
+      .catch(err => {
+        if(formValues.username === '' || formValues.password === '') {
+          setFormError('Username or Password is not valid.')
+        } else {
+          setFormError(err.message)
+        }
+      })
+    }
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <form>
+        <form onSubmit={loginHandler}>
           <input
             type='text'
             name='username'
@@ -51,14 +62,11 @@ const Login = () => {
             value={formValues.password}
           />
 
-          {/* <input
-            type='submit'
-          /> */}
           <button type='submit'>Login</button>
         </form>
       </div>
 
-      <p data-testid="errorMessage" className="error">{formErrors}</p>
+      <p data-testid="errorMessage" className="error">{formError}</p>
     </div>
   );
 };
